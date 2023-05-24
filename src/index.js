e9e721eaaf61cf83195ed6eaf104a43f9a1af407
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-24 23:46:49 +0300

    Add event listener for submit button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+import {
+  displayScores,
+  addGameScore,
+} from './modules/gameFunction.js';
+
+// call displayScores function to display scores stored in the api
+displayScores();
+
+// event listener for submit button
+const submitButton = document.getElementById('btnAddScore');
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addGameScore();
+  displayScores();
+});
