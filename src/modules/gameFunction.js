3acabff0c67cb7068ee66bcc7fb1f01e71dbdc3c
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-24 23:44:49 +0300

    Add displayScores function

--- src/modules/gameFunction.js
+++ src/modules/gameFunction.js
@@ -1,3 +1,17 @@
 const baseURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
 const gameId = '6lbvUiU6NvtGL9BW17VD';
 const url = `${baseURL}games/${gameId}/scores/`;
+
+// Get all game scores from the leaderboard api
+export const displayScores = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const scores = data.result;
+  const scoresList = document.getElementById('scoreList');
+  scoresList.innerHTML = '';
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.textContent = `${score.user}: ${score.score}`;
+    scoresList.appendChild(li);
+  });
+};
